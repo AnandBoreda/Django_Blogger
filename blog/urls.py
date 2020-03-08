937a12039e1483f887cefee68a5b894c50ef839b
AnandBoreda <zz.anand2016@gmail.com>
2020-03-08 13:27:35 +0530

    bug fixes

--- blog/urls.py
+++ blog/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='base'),
-    path('registration.html', views.registration, name='registration'),
+    path('', views.index, name='index'),
+    path('index.html', views.index, name='index'),
     path('about.html', views.about, name='about'),
     path('contact.html', views.contact, name='contact'),
-    path('gallery.html', views.gallery, name='gallery'),
-    path('login.html', views.login, name='login'),
-    path('services.html', views.services, name='services'),
     path('single.html', views.single, name='single'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home(request):
-    return render(request, 'base.html')
+def index(request):
+    return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
@@ -10,18 +10,5 @@ def about(request):
 def contact(request):
     return render(request, 'contact.html')
 
-def gallery(request):
-    return render(request, 'gallery.html')
-
-def login(request):
-    return render(request, 'login.html')
-
-def services(request):
-    return render(request, 'services.html')
-
 def single(request):
     return render(request, 'single.html')
-    
-def registration(request):
-    return render(request, 'registration.html')
-
